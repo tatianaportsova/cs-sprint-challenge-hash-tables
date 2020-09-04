85f7bf25849febcdd32dbe3a2349956c4dcaa2a2
tatianaportsova <tt.sova@gmail.com>
2020-09-04 13:52:57 -0400

    Completed 4 tasks

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -6,22 +6,32 @@ def has_negatives(a):
         if i not in cache:
             cache[i] = abs(i)
 
-    
     dict_ = {}
-    for key1,value1 in cache.items():
-        if key1 not in dict_:
-            for key2,value2 in cache.items():
-                if key2 not in dict_:
-                    if value1 == value2:
-                        if key1 != key2:
-                            if key1 > key2:
-                                # if key1 not in dict_:
-                                dict_[key1] = 1
-                            else:
-                                dict_[key2] = 1
-                            break
+    for key,value in cache.items():
+        if value not in dict_:
+            dict_[value] = 1
+        else:
+            dict_[value] += 1
     
-    result = list(dict_.keys())
+    result = []
+    for key,value in dict_.items():
+        if dict_[key] > 1:
+            result.append(key)
+
+    # dict_ = {}
+    # for key1,value1 in cache.items():
+    #     if key1 not in dict_:
+    #         for key2,value2 in cache.items():
+    #             if key2 not in dict_:
+    #                 if value1 == value2:
+    #                     if key1 != key2:
+    #                         if key1 > key2:
+    #                             dict_[key1] = 1
+    #                         else:
+    #                             dict_[key2] = 1
+    #                         break
+    
+    # result = list(dict_.keys())
 
     return result
 
